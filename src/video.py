68510748c421ea5cbe5a682db3cc8355a0b6da4d
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2023-11-06 13:31:58 +1000

    Отредактированный код задания

--- src/video.py
+++ src/video.py
@@ -5,17 +5,25 @@ class Video(ApiMixin):
 
     def __init__(self, video_id):
         self.__video_id = video_id
-        self.video = self.get_service().videos().list(
+        try:
+            self.video = self.get_service().videos().list(
             id=video_id, part='snippet,statistics'
-        ).execute()
-        self.id = self.video['items'][0]['id']
-        self.title_video = self.video['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.__video_id}'
-        self.video_count = int(self.video['items'][0]['statistics']['viewCount'])
-        self.video_likes = int(self.video['items'][0]['statistics']['likeCount'])
+            ).execute()
+            self.id = self.video['items'][0]['id']
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.__video_id}'
+            self.view_count = int(self.video['items'][0]['statistics']['viewCount'])
+            self.like_count = int(self.video['items'][0]['statistics']['likeCount'])
+
+        except(IndexError, TypeError):
+            self.title = self.url = self.view_count = self.like_count = None
 
     def __str__(self):
-        return f'{self.title_video}'
+        return f'{self.title}'
+
+    @property
+    def video_id(self) -> str:
+        return self.__video_id
 
 
 class PLVideo(Video):
